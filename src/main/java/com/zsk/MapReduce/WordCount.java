a80a19d5a5c23a4d85cfda84ef9f2582fb74b6bd
zhangshoukang <zhangshoukang@fancydigital.com.cn>
2018-09-14 12:03:09 +0800

    add wordcount test cluster

--- src/main/java/com/zsk/MapReduce/WordCount.java
+++ src/main/java/com/zsk/MapReduce/WordCount.java
@@ -1,19 +1,14 @@
 package com.zsk.MapReduce;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -21,52 +16,51 @@ import java.util.StringTokenizer;
 /**
  * Create by zsk on 2018/9/13
  **/
-public class WordCount extends Configured implements Tool {
-    public static class MyMapper extends Mapper<LongWritable,Text,Text,IntWritable>{
+public class WordCount {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
-        private IntWritable one =new IntWritable(1);
 
-        @Override
-        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer stringTokenizer = new StringTokenizer(value.toString());
-            while (stringTokenizer.hasMoreTokens()){
-                word.set(stringTokenizer.nextToken());
-                context.write(word,one);
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
             }
-
         }
     }
-    public static class MyReducer extends Reducer<Text,IntWritable,Text,IntWritable>{
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
         private IntWritable result = new IntWritable();
-        @Override
-        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum=0;
-            for (IntWritable value : values) {
-                sum+=value.get();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
             }
             result.set(sum);
-            context.write(key,result);
+            context.write(key, result);
         }
     }
-    @Override
-    public int run(String[] args) throws Exception{
-        Job job = Job.getInstance(this.getConf(), "WordCount");
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(WordCount.class);
-        job.setInputFormatClass(FileInputFormat.class);
-        job.setMapperClass(MyMapper.class);
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
-        job.setReducerClass(MyReducer.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-        FileInputFormat.addInputPath(job,new Path(args[0]));
-        FileOutputFormat.setOutputPath(job,new Path(args[1]));
-        return job.waitForCompletion(true)?0:1;
-    }
-
-    public static void main(String[] args) throws Exception{
-        int status = ToolRunner.run(new Configuration(), new WordCount(), args);
-        System.exit(status);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
