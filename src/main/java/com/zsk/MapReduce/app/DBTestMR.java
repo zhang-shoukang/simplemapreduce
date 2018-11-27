85ffa3b125cd5af76302e45ffa58e1dbc276b004
zhangshoukang <zhangshoukang@fancydigital.com.cn>
2018-11-27 18:22:22 +0800

    add db input output test

--- src/main/java/com/zsk/MapReduce/app/DBTestMR.java
+++ src/main/java/com/zsk/MapReduce/app/DBTestMR.java
@@ -5,10 +5,12 @@ import com.zsk.MapReduce.mapreduce.MyDBTestMapper;
 import com.zsk.MapReduce.mapreduce.MyDBTestReducer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.filecache.DistributedCache;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -21,7 +23,9 @@ public class DBTestMR extends Configured implements Tool {
     }
     @Override
     public int run(String[] args) throws Exception {
-        Job job = Job.getInstance(new Configuration(), this.getClass().getSimpleName() + new Date());
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration, this.getClass().getSimpleName() + new Date());
+        DistributedCache.addFileToClassPath(new Path("/tmp/jars/mysql-connector-java-5.1.6.jar"),configuration);
         job.setJarByClass(this.getClass());
         job.setMapperClass(MyDBTestMapper.class);
         job.setReducerClass(MyDBTestReducer.class);
@@ -33,7 +37,7 @@ public class DBTestMR extends Configured implements Tool {
 
         job.setInputFormatClass(org.apache.hadoop.mapreduce.lib.db.DBInputFormat.class);
         job.setOutputFormatClass(org.apache.hadoop.mapreduce.lib.db.DBOutputFormat.class);
-        org.apache.hadoop.mapreduce.lib.db.DBConfiguration.configureDB(job.getConfiguration(), "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/mapreduce",
+        org.apache.hadoop.mapreduce.lib.db.DBConfiguration.configureDB(job.getConfiguration(), "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/testmr",
                 "root", "root");
         org.apache.hadoop.mapreduce.lib.db.DBInputFormat.setInput(job, StudentinfoRecord.class, "select id,name from t","select count(1) from t");
         org.apache.hadoop.mapreduce.lib.db.DBOutputFormat.setOutput(job,"t2",StudentinfoRecord.FIELDS);
